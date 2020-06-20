6045fbd1c249e5b4215927f580892da7a6219617
cheesyorange <67108492+cheesyorange@users.noreply.github.com>
2020-06-20 14:12:07 +0530

    Add files via upload

---
+++ helloprograms-copy/number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class number
+{
+     void main()
+     {
+     double num;
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter a decimal number:");
+     num=sc.nextDouble();
+     double ans= Math.sqrt(num);
+     System.out.println("sqr root of your no is:"+ans);
+     System.out.println("your no was:" +num);
+     
+    }
+    
+}
+
+     
+     
+     
+     
